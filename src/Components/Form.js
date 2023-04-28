e191ca492c4fa9a90d4809412eb4f2d678e7f386
Matt <mmaguire333@gmail.com>
2023-04-28 13:28:05 -0600

    Create skeleton for the Form component

---
+++ src/Components/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <form></form>
+            </div>
+        );
+    }
+}
+
+
+export default Form;
